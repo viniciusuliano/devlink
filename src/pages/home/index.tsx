685dffe4e8f4aa6faada027bfded1147ba3f55b8
Vinicius Uliano <vgarciauliano@gmail.com>
2024-07-15 12:08:27 -0300

    Project ajusted

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,10 @@ export function Home(){
         <div className="flex flex-col w-full py-4 items-center justify-center">
             <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">Vinicius Uliano</h1>
             <span className="text-gray-50 mb-5 mt-5">Veja meus links</span>
+        
+        <main>
+            <h2>Hello</h2>
+        </main>
         </div>
     )
 }
